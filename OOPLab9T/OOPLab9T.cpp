ac4b30d68f0803b213895e65d4b28156bb86d714
Kukuliak-Dmytro <kukuliak.dmytro.clg@chnu.edu.ua>
2024-04-21 18:14:26 +0300

    LR3 finished

--- OOPLab9T/OOPLab9T.cpp
+++ OOPLab9T/OOPLab9T.cpp
@@ -1,4 +1,8 @@
 ﻿#include <iostream>
+#include <list>
+#include <iterator>
+#include <algorithm>
+#include <map>
 #include "School.h"
 
 struct ob {
@@ -84,56 +88,81 @@ void min(){
     }
 }
 */
-#include <list>
-#include <iterator>
-#include <algorithm>
-int main() {
-    setlocale(LC_CTYPE, "ukr");
-    try {
-        std::list<int> L{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
-        std::list<int> L1{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
-        int k;
-        std::cout << "Введiть значення k (0 < k < 5): ";
-        std::cin >> k;
 
-        if (k < 0 || k > 5) {
-            throw std::invalid_argument("k повинно бути в дiапазонi [0, 5]");
-        }
-       //Логічно правильний код, який так і не запрацював
-       /* auto begin = L.end();
-        std::advance(begin, -4);
-        auto end = L.end();
-       // std::advance(end, -5);
-        auto  val = L.begin();
-        std::advance(val, k);*/
-        //в цьому контексті val це ітератор на елемент, який май стати першим
-        auto begin = L.begin();
-        auto end = L.begin();
-        std::advance(end, 5);
-        auto val = L.begin();
-        std::advance(val, k);
-        // Копіюємо елементи з кінця списку на початок списку з циклічним зсувом на k позицій вліво
-        std::rotate_copy(begin, val, end, std::front_inserter(L1));
-
-        begin = L.begin();
-        end = L.begin();
-        std::advance(end, 5);
-        val = L.begin();
-        std::advance(val, k);
-        // Копіюємо елементи з початку списку в кінець списку з циклічним зсувом на k позицій вправо
-        std::rotate_copy(begin, val, end, std::back_inserter(L1));
-
-
-        // Виводимо результат
-        std::cout << "Результат після операцій:" << std::endl;
-        for (const auto& elem : L1) {
-            std::cout << elem << " ";
-        }
-        std::cout << std::endl;
+//int main() {
+//    setlocale(LC_CTYPE, "ukr");
+//    try {
+//        std::list<int> L{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+//        std::list<int> L1{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+//        int k;
+//        std::cout << "Введiть значення k (0 < k < 5): ";
+//        std::cin >> k;
+//
+//        if (k < 0 || k > 5) {
+//            throw std::invalid_argument("k повинно бути в дiапазонi [0, 5]");
+//        }
+//       //Логічно правильний код, який так і не запрацював
+//       /* auto begin = L.end();
+//        std::advance(begin, -4);
+//        auto end = L.end();
+//       // std::advance(end, -5);
+//        auto  val = L.begin();
+//        std::advance(val, k);*/
+//        //в цьому контексті val це ітератор на елемент, який май стати першим
+//        auto begin = L.begin();
+//        auto end = L.begin();
+//        std::advance(end, 5);
+//        auto val = L.begin();
+//        std::advance(val, k);
+//        // Копіюємо елементи з кінця списку на початок списку з циклічним зсувом на k позицій вліво
+//        std::rotate_copy(begin, val, end, std::front_inserter(L1));
+//
+//        begin = L.begin();
+//        end = L.begin();
+//        std::advance(end, 5);
+//        val = L.begin();
+//        std::advance(val, k);
+//        // Копіюємо елементи з початку списку в кінець списку з циклічним зсувом на k позицій вправо
+//        std::rotate_copy(begin, val, end, std::back_inserter(L1));
+//
+//
+//        // Виводимо результат
+//        std::cout << "Результат після операцій:" << std::endl;
+//        for (const auto& elem : L1) {
+//            std::cout << elem << " ";
+//        }
+//        std::cout << std::endl;
+//    }
+//    catch (const std::exception& e) {
+//        std::cerr << "Помилка: " << e.what() << std::endl;
+//    }
+//
+//    return 0;
+//}
+//Дан вектор V.Определить количество повторений каждого числа в векторе V и вывести все различные элементы вектора V вместе с количеством их повторений(в порядке возрастания значений элементов); количество повторений выводить сразу после значения соответствующего
+//элемента.Использовать вспомогательное отображение M
+//(класс map), ключами которого являются различные элементы вектора V, а значениями — количество повторений этих
+//элементов.При заполнении отображения M не использовать
+//условные конструкции(достаточно операций индексирования[] и инкремента).Элементы вектора V(при заполнении
+//    отображения M) и элементы отображения M(при выводе полученных результатов) перебирать в цикле с параметромитератором соответствующего контейнера.
+int main() 
+{
+    std::map<int,int> Map{};
+    const int N = 100;
+    std::vector<int> V{};
+    srand(time(NULL));
+    for (int i = 0; i < N; i++) {
+        V.push_back((rand() % 21) - 10);
     }
-    catch (const std::exception& e) {
-        std::cerr << "Помилка: " << e.what() << std::endl;
-    }
+    for (auto& i : V) {
+        std::cout << i << " ";
+        Map.insert_or_assign(i,Map[i]+1);
 
-    return 0;
+    }
+    std::cout << std::endl;
+    for (auto& i : Map) {
+        
+        std::cout << i.first << ": " << i.second << std::endl;
+    }
+    
 }
